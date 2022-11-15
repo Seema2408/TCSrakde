d6c6ffd2e6e54ec710f4b0069b3edb16a4912088
Seema2408 <118239359+Seema2408@users.noreply.github.com>
2022-11-15 10:53:37 +0530

    Update a.java

--- src/parameterization/a.java
+++ src/parameterization/a.java
@@ -11,5 +11,6 @@ public static void main(String[] args) throws Throwable {
 	FileInputStream fis =new FileInputStream("C:\\Users\\seema\\OneDrive\\Desktop\\New folder\\Family.xlsx");
 	double value = WorkbookFactory.create(fis).getSheet("Sheet1").getRow(0).getCell(0).getNumericCellValue();
 System.out.println(value);
+	System.out.println(Seema);
 }
 }
